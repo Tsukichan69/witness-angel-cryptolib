79011890d2b0921b7fd04d0d2c83494ff9be4f8d
Tsukichan69 <lezere.gwenaelle@hotmail.fr>
2021-02-24 13:39:31 +0100

    Add Shamir Schema and convert to Json_schema

--- pocs/schema_tester.py
+++ pocs/schema_tester.py
@@ -5,7 +5,7 @@ from schema import Schema, Optional, Or, SchemaError
 from uuid import UUID
 import json
 
-
+""" Schema = Creation of a Schema for the containers, depending on the level of complexity, some elements are optional, it seems important to specify that a list can be empty in some cases"""
 SCHEMA_CONTAINER = Schema({
     "data_encryption_strata" : [{
         "data_encryption_algo": str,
@@ -24,9 +24,6 @@ SCHEMA_CONTAINER = Schema({
                                 Optional("keychain_uid"): object}],
         }]
 })
-
-json_schema = SCHEMA_CONTAINER.json_schema("Schema_container")
-print(json_schema);
 
 SIMPLE_CONTAINER_CONF = dict(
     data_encryption_strata=[
@@ -75,6 +72,7 @@ COMPLEX_CONTAINER_CONF = dict(
     ]
 )
 
+json_container_schema = SCHEMA_CONTAINER.json_schema("Schema_container")
 
 
 def check(conf_schema, conf):
@@ -88,41 +86,29 @@ def check(conf_schema, conf):
 
 print(check(SCHEMA_CONTAINER, COMPLEX_CONTAINER_CONF))
 
-
-""" SCHEMA_SIMPLE_SHAMIR = Schema({
+SCHEMA_SHAMIR_CONTAINER = Schema({
     "data_encryption_strata": [{
         "data_encryption_algo": str,
         "key_encryption_strata": [{
                                     "key_encryption_algo": str,
-                                    "key_escrow": {"escrow_type": str}},
-                                {
-                                    "key_encryption_algo": str,
-                                    "key_shared_secret_threshold":int,
-                                    "key_shared_secret_escrows": [{
-                                        "key_encryption_strata": [{
-                                            "key_encryption_algo": str,
-                                            "key_escrow": {"escrow_type": str}}],
-                                        "key_encryption_strata": [{
-                                            "key_encryption_algo": str,
-                                            "key_escrow": {"escrow_type": str}}],
-                                        "key_encryption_strata": [{
-                                            "key_encryption_algo": str,
-                                            "key_escrow": {"escrow_type": str}}],
-                                        "key_encryption_strata": [{
-                                            "key_encryption_algo": str,
-                                            "key_escrow": {"escrow_type": str}}],
+                                    Optional("key_shared_secret_threshold"): int,
+                                    Optional("key_escrow"): {"escrow_type": str},
+                                    Optional("key_shared_secret_escrows"): [{
                                         "key_encryption_strata": [{
                                             "key_encryption_algo": str,
                                             "key_escrow": {"escrow_type": str},
-                                            "keychain_uid": object,}]
-                                            }]
-                                }],
-        "data_signatures": [{"message_digest_algo": str,
+                                            Optional("keychain_uid"): object
+                                        }]
+                                    }]}],
+        Or("data_signatures", list): [{"message_digest_algo": str,
                             "signature_algo": str,
-                            "signature_escrow": {"escrow_type": str}}]
+                            "signature_escrow": {"escrow_type": str},
+                            Optional("keychain_uid"): object},
+                            {Optional("message_digest_algo"): str,
+                            Optional("signature_algo"):str,
+                            Optional("signature_escrow"): {"escrow_type": str}}]
     }]
     })
-
 
 SIMPLE_SHAMIR_CONTAINER_CONF = dict(
     data_encryption_strata=[
@@ -143,7 +129,7 @@ SIMPLE_SHAMIR_CONTAINER_CONF = dict(
                         dict(key_encryption_strata=[
                                 dict(key_encryption_algo="RSA_OAEP", key_escrow=LOCAL_ESCROW_MARKER)],),
                         dict(key_encryption_strata=[
-                                dict(key_encryption_algo="RSA_OAEP", key_escrow=LOCAL_ESCROW_MARKER, keychain_uid=ENFORCED_UID1)],),
+                                dict(key_encryption_algo="RSA_OAEP", key_escrow=LOCAL_ESCROW_MARKER, keychain_uid=UUID("0e8e861e-f0f7-e54b-18ea-34798d5daaaa"))],),
                     ],
                 ),
             ],
@@ -152,8 +138,58 @@ SIMPLE_SHAMIR_CONTAINER_CONF = dict(
             ],
         )
     ]
-) """
-""" print(check(SCHEMA_SIMPLE_SHAMIR, SIMPLE_SHAMIR_CONTAINER_CONF)) """
+) 
+
+COMPLEX_SHAMIR_CONTAINER_CONF = dict(
+    data_encryption_strata=[
+        dict(
+            data_encryption_algo="AES_EAX",
+            key_encryption_strata=[dict(key_encryption_algo="RSA_OAEP", key_escrow=LOCAL_ESCROW_MARKER)],
+            data_signatures=[],
+        ),
+        dict(
+            data_encryption_algo="AES_CBC",
+            key_encryption_strata=[dict(key_encryption_algo="RSA_OAEP", key_escrow=LOCAL_ESCROW_MARKER)],
+            data_signatures=[
+                dict(message_digest_algo="SHA3_512", signature_algo="DSA_DSS", signature_escrow=LOCAL_ESCROW_MARKER)
+            ],
+        ),
+        dict(
+            data_encryption_algo="CHACHA20_POLY1305",
+            key_encryption_strata=[
+                dict(
+                    key_encryption_algo=SHARED_SECRET_MARKER,
+                    key_shared_secret_threshold=2,
+                    key_shared_secret_escrows=[
+                        dict(key_encryption_strata=[
+                                 dict(key_encryption_algo="RSA_OAEP", key_escrow=LOCAL_ESCROW_MARKER),
+                                 dict(key_encryption_algo="RSA_OAEP", key_escrow=LOCAL_ESCROW_MARKER)],),
+                        dict(key_encryption_strata=[
+                                 dict(key_encryption_algo="RSA_OAEP", key_escrow=LOCAL_ESCROW_MARKER)],),
+                        dict(key_encryption_strata=[
+                                 dict(key_encryption_algo="RSA_OAEP", key_escrow=LOCAL_ESCROW_MARKER)],),
+                        dict(key_encryption_strata=[
+                                 dict(key_encryption_algo="RSA_OAEP", key_escrow=LOCAL_ESCROW_MARKER, keychain_uid=UUID("65dbbe4f-0bd5-4083-a274-3c76efeebbbb"))],),
+                    ],
+                )
+            ],
+            data_signatures=[
+                dict(
+                    message_digest_algo="SHA3_256",
+                    signature_algo="RSA_PSS",
+                    signature_escrow=LOCAL_ESCROW_MARKER,
+                    keychain_uid=UUID("0e8e861e-f0f7-e54b-18ea-34798d5daaaa"),
+                ),
+                dict(message_digest_algo="SHA512", signature_algo="ECC_DSS", signature_escrow=LOCAL_ESCROW_MARKER),
+            ],
+        ),
+    ]
+)
+
+print(check(SCHEMA_SHAMIR_CONTAINER, COMPLEX_SHAMIR_CONTAINER_CONF))
+
+json_shamir_container_schema = SCHEMA_SHAMIR_CONTAINER.json_schema("Schema_container")
+
 
 
 
